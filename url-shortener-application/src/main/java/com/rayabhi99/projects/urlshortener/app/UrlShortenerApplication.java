7a0727adc91e6cf341f97436efed1fcc00bdfc4f
rayabhisek22 <rayabhisek22@gmail.com>
2023-08-27 15:09:42 +0530

    Created the basic structure of maven application

---
+++ url-shortener-application/src/main/java/com/rayabhi99/projects/urlshortener/app/UrlShortenerApplication.java
@@ -0,0 +1,12 @@
+package com.rayabhi99.projects.urlshortener.app;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * This is the starting point of the Url Shortener Application
+ */
+public class UrlShortenerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApplication.class, args);
+    }
+}
